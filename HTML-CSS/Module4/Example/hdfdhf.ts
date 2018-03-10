2cb47317eddcbda3467192b92c613912dba986b1
Iryna Keis <irinakeisss@gmail.com>
2018-03-11 00:35:28 +0200

    Remove duplicated directory

--- HTML-CSS/Module4/Example/hdfdhf.ts
+++
@@ -1,5 +0,0 @@
-// .wrapper>.container>.header>a.logo+nav.nav>ul>li.nav-item*4>a.nav-link{label}
-//
-// main.main>.wrapper-info>.container>section.info>h2.title+p.info-text
-//
-//     .wrapper-team>.container>section.team>h2.title+ul.team-list>li.team-item*3>img.team-picture+h3.team-title+span.yeam-text^^button.btn
